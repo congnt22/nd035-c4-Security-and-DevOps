511f32f17b67d952662bbbc0269d7d9b14ea5f96
Cong Nguyen <congnt242@gmail.com>
2023-12-31 20:44:58 +0700

    Upload final project

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,32 +24,70 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private CartRepository cartRepository;
-
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		log.info("Retrieving user by id.");
 		return ResponseEntity.of(userRepository.findById(id));
 	}
-	
+
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+		if (user == null) {
+			log.info("Error 404 - user not found.");
+			return ResponseEntity.notFound().build();
+		}
+		log.info("User found.");
+		return ResponseEntity.ok(user);
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		String username = createUserRequest.getUsername();
+		String password = createUserRequest.getPassword();
+		String confirmPassword = createUserRequest.getConfirmPassword();
+
+		if (username == null || password == null || confirmPassword == null) {
+			log.error("Failed to create user - null parameter");
+			return ResponseEntity.badRequest().build();
+		}
+		if (username.length() < 6) {
+			log.info("Username must be at least 6 characters in length");
+			return ResponseEntity.badRequest().build();
+		}
+		if (username.length() > 15) {
+			log.info("Username must be at most 15 characters in length");
+			return ResponseEntity.badRequest().build();
+		}
+		if (userRepository.findByUsername(username) != null) {
+			log.info("Username taken");
+			return ResponseEntity.badRequest().build();
+		}
+		if (password.length() < 8) {
+			log.info("Username must be at least 8 characters in length");
+			return ResponseEntity.badRequest().build();
+		}
+		if (!password.equals(confirmPassword)) {
+			log.info("Please confirm your password");
+			return ResponseEntity.badRequest().build();
+		}
+
 		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
+		user.setUsername(username);
+		user.setPassword(bCryptPasswordEncoder.encode(password));
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
 		userRepository.save(user);
+		log.info("User created");
 		return ResponseEntity.ok(user);
 	}
 	
---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()                 // Allowing CORS requests
+                .and()
+                .csrf().disable()       // Disable CSRF protection
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTCreationFilter(authenticationManager()))
+                .addFilter(new JWTVerificationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
